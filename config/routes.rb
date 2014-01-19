186475ddf6c6dca4750a4a94507e306414ce3ad6
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-01-19 16:15:09 +0000

    Generate User model and allow login with either username or email

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Issuet::Application.routes.draw do
   root 'home#index'
 
+  devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
