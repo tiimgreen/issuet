4ef2eecac3885e66201a345e4594338befec536e
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-01-19 13:23:32 +0000

    Generate and test root controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Issuet::Application.routes.draw do
+  root 'home#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
