689dfb42699a40feb9f272c70538e56de97bc04f
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-01-21 20:35:38 +0000

    Setup Devise mailer (boilerplate settings)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,14 @@ Issuet::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: '',
+  #   port: 0,
+  #   domain: '',
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true,
+  #   user_name: '',
+  #   password: ''
+  # }
 end
