e67459bfcfd4af07fa8b641ebab271da7735fd09
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-01-22 17:38:54 +0000

    Change roots for authed and non-authed users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Issuet::Application.routes.draw do
-  root 'home#index'
-
   devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
