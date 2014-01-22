70fcafe4027c64c3da7dba3fa1ff0490b8995994
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-01-22 17:44:22 +0000

    Rename 'home' controller to 'dashboard'

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Issuet::Application.routes.draw do
+  get "dashboard/index"
   devise_for :users
   devise_scope :user do
     authenticated :user do
-      root 'home#index', as: :authenticated_root
+      root 'dashboard#index', as: :authenticated_root
     end
     unauthenticated :user do
       root 'devise/sessions#new', as: :unauthenticated_root
