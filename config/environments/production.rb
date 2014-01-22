43627c4c4d767dc8456bc859cc349553b997f462
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-01-22 16:48:27 +0000

    Fix remaining config/ offences (#2)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,8 @@ Issuet::Application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  # Enable serving of images, stylesheets, and JavaScripts from an
+   # asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets.
