7114ec84c0e33a484de1702f14158fb621944233
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-01-22 20:15:50 +0000

    Modify User model specs to use shoulda (#3)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,35 +1,23 @@
 require 'spec_helper'
 
 describe User do
+  it { should have_many(:issues) }
+
   it 'should have a valid factory' do
     FactoryGirl.create(:user).should be_valid
   end
 
-  it 'should require a username' do
-    FactoryGirl.build(:user, username: nil).should_not be_valid
-  end
+  it { should validate_presence_of(:username) }
 
-  it 'should require an email' do
-    FactoryGirl.build(:user, email: nil).should_not be_valid
-  end
+  it { should validate_presence_of(:email) }
 
-  it 'should require a password and confirmation' do
-    FactoryGirl.build(
-      :user, password: nil, password_confirmation: nil).should_not be_valid
-  end
+  it { should validate_presence_of(:password) }
 
-  it 'should require email to match email address format' do
-    FactoryGirl.build(:user, email: 'rafalchmiel').should_not be_valid
-    FactoryGirl.build(:user, email: 'hi@rafalchmiel.com').should be_valid
-  end
+  it { should allow_value('hi@rafalchmiel.com').for(:email) }
 
-  it 'should not allow duplicate usernames' do
-    FactoryGirl.create(:user, username: 'rafalchmiel')
-    FactoryGirl.build(:user, username: 'rafalchmiel').should_not be_valid
-  end
+  it { should_not allow_value('rafalchmiel').for(:email) }
 
-  it 'should not allow duplicate emails' do
-    FactoryGirl.create(:user, email: 'hi@rafalchmiel.com')
-    FactoryGirl.build(:user, email: 'hi@rafalchmiel.com').should_not be_valid
-  end
+  it { should validate_uniqueness_of(:username) }
+
+  it { should validate_uniqueness_of(:email) }
 end
