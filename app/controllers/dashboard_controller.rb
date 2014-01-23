7954251186cfb4bd736763109578ba7fe7d03745
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-01-23 17:07:21 +0000

    Prefer redirect solution in controller not in routes (easier to test)

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
   def index
+    user_signed_in? ? render(:index) : redirect_to(:new_user_session)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Issuet::Application.routes.draw do
+  root 'dashboard#index'
+
   devise_for :users
-  devise_scope :user do
-    authenticated :user do
-      root 'dashboard#index', as: :authenticated_root
-    end
-    unauthenticated :user do
-      root 'devise/sessions#new', as: :unauthenticated_root
-    end
-  end
 end
