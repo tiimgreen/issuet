ff35f8f47b193bbb09cea0ea9776b57c400510cd
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-01-23 21:12:59 +0000

    Remove unnecessary new line

--- spec/factories/issues.rb
+++ spec/factories/issues.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :issue do
     title { Faker::Lorem.sentence }
     body { Faker::Lorem.paragraph }
-
     user { FactoryGirl.create(:user) }
     project { FactoryGirl.create(:project) }
   end
