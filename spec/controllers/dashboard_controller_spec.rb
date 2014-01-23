a1ff008cff9ef74f68d42ed6af1fd5bd6b6eedf2
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-01-23 16:23:12 +0000

    Write Dashboard controller specs (#3)

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,4 +1,16 @@
 require 'spec_helper'
 
 describe DashboardController do
+  describe 'GET #index' do
+    it 'should respond successfully with a HTTP 200 status code' do
+      get :index
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+
+    it 'should render the "index" template' do
+      get :index
+      expect(response).to render_template('index')
+    end
+  end
 end
