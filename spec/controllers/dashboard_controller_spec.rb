760c57fd570cabe69efcc2a0eab68546160111ac
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-01-23 17:26:27 +0000

    Write more sophisticated Dashboard controller specs (#3)

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,15 +2,20 @@ require 'spec_helper'
 
 describe DashboardController do
   describe 'GET #index' do
-    it 'should respond successfully with a HTTP 200 status code' do
-      get :index
-      expect(response).to be_success
-      expect(response.status).to eq(200)
+    context 'when user signed in' do
+      login_user!
+
+      it 'should render the "index" template' do
+        get :index
+        expect(response).to render_template('index')
+      end
     end
 
-    it 'should render the "index" template' do
-      get :index
-      expect(response).to render_template('index')
+    context 'when user not signed in' do
+      it 'should redirect to the sign in view' do
+        get :index
+        expect(response).to redirect_to('/users/sign_in')
+      end
     end
   end
 end
