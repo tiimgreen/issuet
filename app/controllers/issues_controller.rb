bbd799c13e13d6bc98f57f791e72462c6c81b56f
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-01-26 13:44:45 +0000

    Implement creation of issues

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -9,4 +9,32 @@ class IssuesController < ApplicationController
   def new
     @issue = Issue.new
   end
+
+  def create
+    @user = User.find(current_user)
+    @project = Project.find_by_name(params[:project_id])
+    @issue = @project.issues.new(permit)
+    @issue.user = @user
+
+    if @issue.save
+      redirect_to project_issues_path(@project)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def issue_owner?
+    @issue = Issue.find_by_name(params[:id])
+    redirect_to @issue unless @issue.user == current_user
+  end
+
+  def issue_params
+    params.require(:issue).permit(:title, :body)
+  end
+
+  def permit
+    issue_params.permit(:title, :body)
+  end
 end
