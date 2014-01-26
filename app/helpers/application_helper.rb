4b98c4e612f21c4e1324f492e872f0c86cda4bee
Tim Green <tiimgreen@users.noreply.github.com>
2014-01-26 18:27:03 +0000

    Use bullet instead of pipe

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,6 @@ module ApplicationHelper
 
   def full_title(page_title)
     base_title = 'Issuet'
-    page_title.empty? ? base_title : "#{base_title} | #{page_title}"
+    page_title.empty? ? base_title : "#{base_title} · #{page_title}"
   end
 end
