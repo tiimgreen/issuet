7a50d408e673231a3448caff9548768e4badaf1f
Tim Green <tiimgreen@users.noreply.github.com>
2014-01-26 18:15:16 +0000

    Updated for single quotes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
   end
 
   def full_title(page_title)
-    base_title = "Issuet"
+    base_title = 'Issuet'
     page_title.empty? ? base_title : "#{base_title} | #{page_title}"
   end
 end
