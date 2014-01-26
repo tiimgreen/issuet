97940f475d0e8ce38b4e25dac37230d2bde1cdaa
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-01-26 19:23:06 +0000

    Write Issue controller specs for #index and #new

--- spec/controllers/issues_controller_spec.rb
+++ spec/controllers/issues_controller_spec.rb
@@ -1,5 +1,41 @@
 require 'spec_helper'
 
 describe IssuesController do
+  before(:each) { @project = FactoryGirl.build(:project) }
 
+  describe 'GET #index' do
+    context 'when user signed in' do
+      login_user!
+
+      it 'should redirect to the Project show view' do
+        get :index, project_id: @project.name
+        expect(response).to redirect_to("/projects/#{@project.name}")
+      end
+    end
+
+    context 'when user not signed in' do
+      it 'should redirect to the sign in view' do
+        get :index, project_id: @project.name
+        expect(response).to redirect_to('/users/sign_in')
+      end
+    end
+  end
+
+  describe 'GET #new' do
+    context 'when user signed in' do
+      login_user!
+
+      it 'should render the "new" template' do
+        get :new, project_id: @project.name
+        expect(response).to render_template('new')
+      end
+    end
+
+    context 'when user not signed in' do
+      it 'should redirect to the sign in view' do
+        get :new, project_id: @project.name
+        expect(response).to redirect_to('/users/sign_in')
+      end
+    end
+  end
 end
