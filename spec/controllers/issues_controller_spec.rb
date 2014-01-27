716be3ecfbb874c14b88a373ab71caa98de667a5
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-01-27 19:19:25 +0000

    Complete Issue controller specs (#10)

--- spec/controllers/issues_controller_spec.rb
+++ spec/controllers/issues_controller_spec.rb
@@ -38,4 +38,54 @@ describe IssuesController do
       end
     end
   end
+
+  describe 'POST #create' do
+    before(:each) do
+      @project = FactoryGirl.create(:project)
+    end
+
+    context 'when user signed in' do
+      login_user!
+
+      it 'should redirect to the newly created project path' do
+        issue = FactoryGirl.build(:issue)
+        post :create, project_id: @project.name, issue: {
+          title: issue.title,
+          body: issue.body
+        }
+        expect(response).to redirect_to("/projects/#{@project.name}/issues")
+      end
+    end
+
+    context 'when user not signed in' do
+      it 'should redirect to the sign in view' do
+        get :new, project_id: @project.name
+        expect(response).to redirect_to('/users/sign_in')
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    before(:each) do
+      @project = FactoryGirl.create(:project)
+      FactoryGirl.create(:issue, project: @project)
+      @first_issue = @project.issues.first
+    end
+
+    context 'when user signed in' do
+      login_user!
+
+      it 'should render the "show" template' do
+        get :show, project_id: @project.name, id: @first_issue
+        expect(response).to render_template('show')
+      end
+    end
+
+    context 'when user not signed in' do
+      it 'should redirect to the sign in view' do
+        get :show, project_id: @project.name, id: @first_issue
+        expect(response).to redirect_to('/users/sign_in')
+      end
+    end
+  end
 end
